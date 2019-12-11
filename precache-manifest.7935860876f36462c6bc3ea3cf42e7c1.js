cdb476a6cd32b31f15a22139d1044476e6140cfb
gtarragona <gtarragona@switchfly.com>
2019-12-11 16:28:43 -0700

    Updates

---
+++ precache-manifest.7935860876f36462c6bc3ea3cf42e7c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55cb179b9369cddef5cab3b1fb1e5edf",
+    "url": "/hnclient/index.html"
+  },
+  {
+    "revision": "c2eb3a2b685a6b39e09a",
+    "url": "/hnclient/static/css/main.dacb0c65.chunk.css"
+  },
+  {
+    "revision": "835a8557e2643d82604e",
+    "url": "/hnclient/static/js/2.64a17f9c.chunk.js"
+  },
+  {
+    "revision": "95c534deb415fe087260dab4786e3626",
+    "url": "/hnclient/static/js/2.64a17f9c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c2eb3a2b685a6b39e09a",
+    "url": "/hnclient/static/js/main.c4f1b805.chunk.js"
+  },
+  {
+    "revision": "5362586bd13984e46063",
+    "url": "/hnclient/static/js/runtime-main.e32d6362.js"
+  }
+]);
